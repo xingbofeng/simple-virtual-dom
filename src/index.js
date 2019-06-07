bc29855902e7776a40395d793a3561c64729a62f
xingbofeng <503908971@qq.com>
2019-06-07 21:52:00 +0800

    更新：基于snabbdom算法的实现

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
+let vKey = 0;
+
 function vdom(type, props, ...children) {
     return {
         type,
-        props,
-        children,
+        props: {
+            ...props,
+            'v-key': ++vKey,
+        },
+        children: children.map(child => {
+            // 对文本节点的特殊处理
+            if (typeof child === 'string') {
+                return {
+                    type: 'textNode',
+                    props: { 'v-key': ++vKey },
+                    children: [],
+                    text: child,
+                };
+            }
+            return child;
+        }),
     };
+}
+
+function sameVnode(vnode1, vnode2) {
+  return vnode1.props['v-key'] === vnode2.props['v-key'];
 }
 
 function toRealDom(vdom) {
     let $dom;
-    if (typeof vdom === 'string') {
-        $dom = document.createTextNode(vdom);
+    if (vdom.type === 'textNode') {
+        $dom = document.createTextNode(vdom.text);
     } else {
         $dom = document.createElement(vdom.type);
     }
 
     if (vdom.props) {
         Object.keys(vdom.props).forEach(key => {
-            setProps($dom, key, vdom.props[key]);
+            vdom.type !== 'textNode' && setProps($dom, key, vdom.props[key]);
         });
     }
 
@@ -27,6 +47,8 @@ function toRealDom(vdom) {
         });
     }
 
+    vdom.$el = $dom;
+
     return $dom;
 }
 
@@ -34,8 +56,25 @@ function mount($parent, vNode) {
     return $parent.appendChild(toRealDom(vNode));
 }
 
-function updateDom($parent, oldNode, newNode, index = 0) {
-    const $currentDom = $parent.childNodes[index];
+function createKeyToOldIdx(children, beginIdx, endIdx) {
+    let i;
+    let map = {};
+    let key;
+    let child;
+    for (i = beginIdx; i <= endIdx; ++i) {
+        child = children[i];
+        if (child != null) {
+            key = child.props['v-key'];
+            if (key !== undefined) {
+                map[key] = i;
+            }
+        }
+    }
+    console.log(children, beginIdx, endIdx, map);
+    return map;
+}
+
+function updateDom($parent, oldNode, newNode) {
     // 没有旧的节点，添加新的节点
     if (!oldNode) {
         return $parent.appendChild(toRealDom(newNode));
@@ -43,20 +82,23 @@ function updateDom($parent, oldNode, newNode, index = 0) {
 
     // 没有新的节点，删除旧的节点
     if (!newNode) {
-        return $parent.removeChild($currentDom);
+        return $parent.removeChild(oldNode.$el);
     }
 
     // 都是文本节点，都没有发生变化
-    if (typeof oldNode === 'string' && typeof newNode === 'string' && oldNode === newNode) {
+    if (oldNode.type === 'textNode' && newNode.type === 'textNode' && oldNode.text === newNode.text) {
         return;
     }
 
     // oldNode和newNode都有值，深入对比
-    if (isNodeChanged(oldNode, newNode)) {
-        return $parent.replaceChild(toRealDom(newNode), $currentDom);
-    }
+    // 根节点发生改变，则不是相同的vNode
+    // 直接replaceChild
+    // if (isNodeChanged(oldNode, newNode)) {
+    //     return $parent.replaceChild(toRealDom(newNode), oldNode.$el);
+    // }
 
     // 虚拟DOM的type未改变，对比节点的props是否改变
+    // 对比props，进行替换vNode
     const oldProps = oldNode.props || {};
     const newProps = newNode.props || {};
     if (isPropsChanged(oldProps, newProps)) {
@@ -66,7 +108,7 @@ function updateDom($parent, oldNode, newNode, index = 0) {
         // 如果新节点没有属性，把旧的节点的属性清除掉
         if (newPropsKeys.length === 0) {
             oldPropsKeys.forEach(propKey => {
-                removeProp($currentDom, propKey, oldProps[propKey]);
+                removeProp(oldNode.$el, propKey, oldProps[propKey]);
             });
         } else {
             // 拿到所有的props，以此遍历，增加/删除/修改对应属性
@@ -74,11 +116,11 @@ function updateDom($parent, oldNode, newNode, index = 0) {
             allPropsKeys.forEach(propKey => {
                 // 属性被去除了
                 if (!newProps[propKey]) {
-                    return removeProp($currentDom, propKey, oldProps[propKey]);
+                    return removeProp(oldNode.$el, propKey, oldProps[propKey]);
                 }
                 // 属性改变了/增加了
-                if (newProps[propKey] !== oldProps[propKey]) {
-                    return setProps($currentDom, propKey, newProps[propKey]);
+                if (newProps[propKey] !== oldProps[propKey] && oldNode.type !== 'textNode') {
+                    return setProps(oldNode.$el, propKey, newProps[propKey]);
                 }
             });
         }
@@ -89,9 +131,89 @@ function updateDom($parent, oldNode, newNode, index = 0) {
         (oldNode.children && oldNode.children.length) ||
         (newNode.children && newNode.children.length)
     ) {
-        for (let i = 0; i < oldNode.children.length || i < newNode.children.length; i++) {
-            updateDom($currentDom, oldNode.children[i], newNode.children[i], i);
+        let oldStartIdx = 0;
+        let oldEndIdx = oldNode.children.length - 1;
+        let oldStartVnode = oldNode.children[oldStartIdx];
+        let oldEndVnode = oldNode.children[oldEndIdx];
+        
+        let newStartIdx = 0;
+        let newEndIdx = newNode.children.length - 1;
+        let newStartVnode = newNode.children[newStartIdx];
+        let newEndVnode = newNode.children[newEndIdx];
+
+        let oldKeyToIdx;
+        let idxInOld;
+        let elmToMove;
+        let before;
+
+        while(oldStartIdx <= oldEndIdx && newStartIdx <= newEndIdx) {
+            if (!oldStartVnode) {
+                oldStartVnode = oldNode.children[++oldStartIdx];
+            } else if (!oldEndVnode) {
+                oldEndVnode = oldNode.children[--oldEndIdx];
+            } else if (!newStartVnode) {
+                newStartVnode = newNode.children[++newStartIdx];
+            } else if (!newEndVnode) {
+                newEndVnode = newNode.children[--newEndIdx];
+            } else if (sameVnode(oldStartVnode, newStartVnode)) { // 如果两个节点是同一个节点，则更新其子节点
+                updateDom(oldNode.$el, oldStartVnode, newStartVnode);
+                oldStartVnode = oldNode.children[++oldStartIdx];
+                newStartVnode = newNode.children[++newStartIdx];
+            } else if (sameVnode(oldEndVnode, newEndVnode)) {
+                updateDom(oldNode.$el, oldEndVnode, newEndVnode);
+                oldEndVnode = oldNode.children[--oldEndIdx];
+                newEndVnode = newNode.children[--newEndIdx];
+            } else if (sameVnode(oldStartVnode, newEndVnode)) {
+                updateDom(oldNode.$el, oldStartVnode, newStartVnode);
+                oldNode.$el.insertBefore(oldStartVnode.$el, oldEndVnode.$el.nextSibling);
+                oldStartVnode = oldNode.children[++oldStartIdx];
+                newEndVnode = newNode.children[--newEndIdx];
+            } else if (sameVnode(oldEndVnode, newStartVnode)) {
+                updateDom(oldNode.$el, oldStartVnode, newStartVnode);
+                oldNode.$el.insertBefore(oldEndVnode.$el, oldStartVnode.$el);
+                oldEndVnode = oldNode.children[--oldEndIdx];
+                newStartVnode = newNode.children[++newStartIdx];
+            } else {
+                // 如果不存在旧节点的key-index表，则创建
+                if (oldKeyToIdx === undefined) {
+                    oldKeyToIdx = createKeyToOldIdx(oldNode.children, oldStartIdx, oldEndIdx);
+                }
+                // 找到新节点在旧节点组中对应节点的位置
+                idxInOld = oldKeyToIdx[newStartVnode.props['v-key']];
+                // 该节点是一个新的节点，则在旧节点前插入该节点
+                if (idxInOld === undefined) {
+                    oldNode.$el.insertBefore(toRealDom(newStartVnode), oldStartVnode.$el);
+                    newStartVnode = newNode.children[++newStartIdx];
+                } else { // 该节点是一个旧节点，则移动该节点
+                    elmToMove = oldNode.children[idxInOld];
+                    updateDom(oldNode.$el, elmToMove, newStartVnode);
+                    // 然后将旧节点组中对应节点设置为undefined,代表已经遍历过了，不在遍历，否则可能存在重复插入的问题
+                    oldNode.children[idxInOld] = undefined;
+                    oldNode.$el.insertBefore(elmToMove.$el, oldStartVnode.$el);
+                }
+                // 自增newStartIdx，继续遍历新节点
+                newStartVnode = newNode.children[++newStartIdx];
+            }
+
+            // 当旧头索引大于旧尾索引时，代表旧节点组已经遍历完，将剩余的新Vnode添加到最后一个新节点的位置后
+            if (oldStartIdx > oldEndIdx) {
+                for (let i = newStartIdx; i <= newEndIdx; i++) {
+                    if (newNode.children[i]) {
+                        oldNode.$el.insertBefore(toRealDom(newNode.children[i]), newNode.children[newEndIdx + 1] ? newNode.children[newEndIdx + 1].$el : null);
+                    }
+                }
+            } else if (newStartIdx > newEndIdx) {
+                for (let i = oldStartIdx; i <= oldEndIdx; i++) {
+                    if (oldNode.children[i]) {
+                        oldNode.$el.removeChild(oldNode.children[i].$el);
+                    }
+                }
+            }
         }
+
+        // for (let i = 0; i < oldNode.children.length || i < newNode.children.length; i++) {
+        //     updateDom(oldNode.$el, oldNode.children[i], newNode.children[i]);
+        // }
     }
 }
 
@@ -146,8 +268,8 @@ function isNodeChanged(oldNode, newNode) {
     }
 
     // 都是textNode，比较文本是否改变
-    if (typeof oldNode === 'string' && typeof newNode === 'string') {
-        return oldNode !== newNode;
+    if (oldNode.type === 'textNode' && newNode.type === 'textNode') {
+        return oldNode.text !== newNode.text;
     }
 
     // 都是element节点，比较节点类型是否改变
@@ -203,7 +325,33 @@ const newNode = (
 
 mount(app, oldNode); // 初始化挂载一个dom
 
-
 setTimeout(() => {
-    updateDom(app, oldNode, newNode); // 更新dom
+    // updateDom(app, oldNode, newNode); // 更新dom
+
+    /**
+     * 测试，在oldNode中添加一个节点
+     */
+    // updateDom(app, oldNode, {
+    //     ...oldNode,
+    //     children: [...oldNode.children, {
+    //         type: 'div',
+    //         props: { 'v-key': ++vKey },
+    //         children: [{
+    //             type: 'textNode',
+    //             text: 'test new node',
+    //             props: { 'v-key': ++vKey },
+    //             children: [],
+    //         }],
+    //     }],
+    // });
+    
+
+    /**
+     * 测试，颠倒oldNode的节点
+     */
+    
+    updateDom(app, oldNode, {
+        ...oldNode,
+        children: [...oldNode.children].reverse(),
+    });
 }, 2000);
